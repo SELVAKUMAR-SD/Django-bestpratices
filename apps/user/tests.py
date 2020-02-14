0d474a3146a0e17abb1a27550d5d28d089eddca0
selvakumar <selvakumar15it@gmail.com>
2020-02-14 12:20:37 +0530

    todo added

--- apps/user/tests.py
+++ apps/user/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestUserRoutes(TestCase):
+    """
+    Testing For User Endpoint's
+    """
+
+    def test_user_signup(self):
+        """
+        Test user signup
+        :return: 201
+        """
+        response = self.client.post('users/signup/',
+                                    data=dict(firtst_name='prime',
+                                              last_name='prime',
+                                              email='selvakumar@gmail.com',
+                                              password='iphone21',
+                                              phone_no='9655952778'))
+        print(response)
